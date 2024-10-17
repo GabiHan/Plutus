2dfd22e18aa8d4c9b52646a1241a088fc9a84429
Gabriella <Xhantanirina@gmail.com>
2024-10-17 19:13:00 +0300

    chore: Update installed app to consider member

--- web/webPlutus/settings.py
+++ web/webPlutus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members' #Add our newly created app 'members' here to load it. Do the same for any futur apps
 ]
 
 MIDDLEWARE = [
