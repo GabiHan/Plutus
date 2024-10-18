a387cdb9ceddaf5c0be58eb11615744858633eab
Gabriella <Xhantanirina@gmail.com>
2024-10-18 21:08:42 +0300

    chore: add user register confi

--- web/members/views.py
+++ web/members/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.forms import UserCreationForm
 
 def members(request):
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
 # Here we are configuring the views of our app to load what's in our index.html
+
+def registerView(request):
+  form = UserCreationForm()
+  return render(request, 'register.html', {"form" : form })
