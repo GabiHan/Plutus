73a4f5707f09cc3f4f1d74d33fc8471348d127fe
Gabriella <gabihantanirina@gmail.com>
2024-10-22 19:39:44 +0300

    chore: added login class for user_auth

--- web/members/views.py
+++ web/members/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import messages 
+from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth import authenticate
 
 
 #Define registration here 
@@ -16,4 +17,16 @@ def register(request):
         form = UserRegistrationForm()
     return render(request, 'members/register.html', {'form': form})
 
+def login(request):
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        password = request.POST.get('password')
+        user = authenticate(request, login=login, password=password)
+        if user is not None :
+            login(request, user)
+            return redirect('home.ht')
+        else:
+            return render(request, 'members/login.html', {'error': 'Invalid credentials.'})
+    else:
+        return render(request, 'members/login.html')
 
