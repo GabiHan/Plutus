6b0c8e6e94fd3a99dd71d7b8b655c2475831ebd1
Gabriella <gabihantanirina@gmail.com>
2024-10-29 21:03:12 +0300

    chore: Use css for enchancement

--- web/webPlutus/settings.py
+++ web/webPlutus/settings.py
@@ -121,14 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = './static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-TAILWIND_APP_NAME = 'home'
  
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
