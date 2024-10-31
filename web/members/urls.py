917de33d2cf5fe072b1dafb2f59b6e36af1b4f81
Gabriella <gabihantanirina@gmail.com>
2024-10-31 16:45:46 +0300

    chore: fix link to member content

--- web/members/urls.py
+++ web/members/urls.py
@@ -18,9 +18,11 @@ urlpatterns = [
 ]
 """
 from django.urls import path
-from .views import register, login
+from .views import register, login_view, user_profile
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('login/', login_view, name='login_view'),
+    path('index/', user_profile, name='user_profile')
+
 ]
