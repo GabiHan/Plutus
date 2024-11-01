10cc9ed2527ed4d4a4620d88d73ade515156a940
Gabriella <gabihantanirina@gmail.com>
2024-11-01 20:10:33 +0300

    chore: added a foreign key setting the user's financial status'

--- web/members/models.py
+++ web/members/models.py
@@ -40,13 +40,22 @@ class Member(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.login  
 
+#Foreign key table for user personal account
 class UserProfile(models.Model):
     user = models.OneToOneField(Member, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
+
+#define a new table, a foreign key of UserProfile that take care of the user input for his wallet 
+class finance(models.Model):
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='financial_entries')
+    money = models.DecimalField(max_digits=10, decimal_places=2)
+    comment = models.TextField()
+    date = models.DateField()
     
 
     def __str__(self):
         return f"{self.user.firstname} {self.user.lastname}"
+        return f"{self.comment} - {self.money} on {self.date}"
 
 
 @receiver(post_save, sender=Member)
