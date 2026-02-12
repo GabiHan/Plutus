69370c6167030d160434b75f1a8cb6f531bb7d15
HANTANIRINA Gabriella <gabihantanirina@gmail.com>
2026-02-12 10:16:20 +0300

    style: style register and login page

--- web/members/forms.py
+++ web/members/forms.py
@@ -1,18 +1,25 @@
 from django import forms
-from .models import Member, MemberManager, UserProfile
+from .models import Member
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter a secure password'})
+    )
 
     class Meta:
         model = Member
-        fields = ['login', 'firstname','age', 'lastname', 'password']  # Add all necessary fields
+        fields = ['login', 'firstname', 'age', 'lastname', 'password']
 
+        widgets = {
+            'login': forms.TextInput(attrs={'placeholder': 'Enter your username'}),
+            'firstname': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'lastname': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+            'age': forms.NumberInput(attrs={'placeholder': 'Your Age'}),
+        }
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password'])  # Ensure password is hashed
+        user.set_password(self.cleaned_data['password'])  # Hash password
         if commit:
             user.save()
         return user
